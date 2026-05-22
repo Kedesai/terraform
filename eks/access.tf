7d133255cfb113c324a705de46a4d152dc870289
kedesai <ketandesai@yahoo.com>
2026-05-21 21:02:32 -0600

    corrected issues in access.tf

--- eks/access.tf
+++ eks/access.tf
@@ -1,6 +1,8 @@
 resource "aws_eks_access_entry" "admin" {
-  cluster_name      = aws_eks_cluster.this.name
-  principal_arn     = aws_iam_role.eks_cluster.arn
+  cluster_name = aws_eks_cluster.this.name
+
+  principal_arn = var.cluster_role_arn != null ? var.cluster_role_arn : aws_iam_role.eks_cluster[0].arn
+
   kubernetes_groups = ["system:masters"]
   type              = "STANDARD"
 }
