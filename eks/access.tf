ffd9d1d9ae613ba3d48d61410bb61404a00a05a8
kedesai <ketandesai@yahoo.com>
2026-05-22 12:48:26 -0600

    fixed syntax error

--- eks/access.tf
+++ eks/access.tf
@@ -5,9 +5,11 @@
 resource "aws_eks_access_entry" "admin" {
   cluster_name = aws_eks_cluster.this.name
 
-  principal_arn = var.cluster_role_arn != null 
-    ? var.cluster_role_arn 
-    : aws_iam_role.eks_cluster[0].arn
+  principal_arn = (
+    var.cluster_role_arn != null ?
+    var.cluster_role_arn :
+    aws_iam_role.eks_cluster[0].arn
+  )
 }
 
 #################################
@@ -17,9 +19,11 @@ resource "aws_eks_access_entry" "admin" {
 resource "aws_eks_access_policy_association" "admin" {
   cluster_name = aws_eks_cluster.this.name
 
-  principal_arn = var.cluster_role_arn != null 
-    ? var.cluster_role_arn 
-    : aws_iam_role.eks_cluster[0].arn
+  principal_arn = (
+    var.cluster_role_arn != null ?
+    var.cluster_role_arn :
+    aws_iam_role.eks_cluster[0].arn
+  )
 
   policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
