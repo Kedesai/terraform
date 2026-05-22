e6760593697ce17bf0d832f86c229dba97092f12
kedesai <ketandesai@yahoo.com>
2026-05-22 12:15:55 -0600

    updated access policy

--- eks/access.tf
+++ eks/access.tf
@@ -3,3 +3,11 @@ resource "aws_eks_access_entry" "admin" {
   cluster_name  = aws_eks_cluster.this.name
   principal_arn = var.cluster_role_arn != null ? var.cluster_role_arn : aws_iam_role.eks_cluster[0].arn
 }
+access_policies {
+
+   policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+   access_scope {
+   type = "cluster"
+    }
+  }
+}
