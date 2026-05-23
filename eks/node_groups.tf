775544c552c2b7119193c94f7e115120588c4ea8
kedesai <ketandesai@yahoo.com>
2026-05-22 21:27:58 -0600

    fixed syntax with subnets

--- eks/node_groups.tf
+++ eks/node_groups.tf
@@ -6,8 +6,11 @@ resource "aws_eks_node_group" "this" {
 
   node_role_arn = var.node_role_arn != null ? var.node_role_arn : aws_iam_role.nodes[0].arn
 
-  subnet_ids = var.subnet_ids
-  ami_type   = "AL2023_x86_64_STANDARD"
+  subnet_ids = [
+    aws_subnet.private_a.id,
+    aws_subnet.private_b.id
+  ]
+  ami_type = "AL2023_x86_64_STANDARD"
 
   scaling_config {
     desired_size = each.value.desired_size
